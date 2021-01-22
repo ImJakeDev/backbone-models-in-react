07e3d621e8d46e2e6ab6657ce410bb59b7c478fc
Jake Schaffer <jake.schaffer@pxdbproject.com>
2021-01-22 16:54:00 -0600

    added extrating data from backbone component

---
+++ src/ExtractingDataFromBackboneModelExample.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Backbone from "backbone";
+
+function connectToBackboneModel(WrappedComponent) {
+  return class BackboneComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = Object.assign({}, props.model.attributes);
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+      this.props.model.on("change", this.handleChange);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      this.setState(Object.assign({}, nextProps.model.attributes));
+      if (nextProps.model !== this.props.model) {
+        this.props.model.off("change", this.handleChange);
+        nextProps.model.on("change", this.handleChange);
+      }
+    }
+
+    componentWillUnmount() {
+      this.props.model.off("change", this.handleChange);
+    }
+
+    handleChange(model) {
+      this.setState(model.changedAttributes());
+    }
+
+    render() {
+      const propsExceptModel = Object.assign({}, this.props);
+      delete propsExceptModel.model;
+      return <WrappedComponent {...propsExceptModel} {...this.state} />;
+    }
+  };
+}
+
+function NameInput(props) {
+  return (
+    <p>
+      <input value={props.firstName} onChange={props.handleChange} />
+      <br />
+      My name is {props.firstName}.
+    </p>
+  );
+}
+
+const BackboneNameInput = connectToBackboneModel(NameInput);
+
+function Example(props) {
+  function handleChange(e) {
+    props.model.set("firstName", e.target.value);
+  }
+
+  return <BackboneNameInput model={props.model} handleChange={handleChange} />;
+}
+
+export default function ExtractingDataFromBackboneModelExample() {
+  const model = new Backbone.Model({ firstName: "Frodo" });
+  return (
+    <center>
+      <h2>Extracting Data from Backbone Model Example</h2>
+      <Example model={model} />
+    </center>
+  );
+}
--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,30 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import BackboneModelExample from "./BackboneModelExample"
+import ExtractingDataFromBackboneModelExample from "./ExtractingDataFromBackboneModelExample";
 import logo from "./logo.svg";
 
 export default function Nav () {
   return (
     <Router>
       <header className="App-header">
+        <br />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Link to="/">Home</Link>
         <Link to="/backbone-model-example">Backbone Model Example</Link>
+        <Link to="/extracting-data-from-backbone-model-example">
+          Extracting Data from Backbone Model Example
+        </Link>
+        <br />
       </header>
       <Switch>
         <Route path="/backbone-model-example">
           <BackboneModelExample />
+        </Route>
+        <Route path="/extracting-data-from-backbone-model-example">
+          <ExtractingDataFromBackboneModelExample />
         </Route>
         <Route path="/">
           <Home />
